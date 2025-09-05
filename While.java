e34a254c84fe1b4b74d2124357522ff9bafc5cff
laura flor <lauraflorrodrigues@gmail.com>
2025-09-05 16:17:31 -0300

    Add files via upload

---
+++ While.java
@@ -0,0 +1,73 @@
+package primeiroProjeto;
+
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		
+		int numero = 1;
+		while (numero <= 10) {
+			System.out.println(numero);
+			numero++;
+		}
+		
+		int numero2 = 10;
+		while (numero2 >= 1) {
+			System.out.println(numero2);
+			numero2--;
+		}
+		
+		int numero3 = 0;
+		while (numero3 <= 100) {
+			System.out.println(numero3);
+			numero3+=5;
+		}
+		
+		int numero4 = 1;
+		while (numero4 <= 5) {
+			System.out.println("Eu gosto de java");
+			numero4++;
+		}
+		
+		int soma = 0;
+		int valor;
+		int contador = 1;
+		
+		System.out.println("Digite 5 números: ");
+		while (contador <= 5) {
+			valor = scanner.nextInt();
+			soma += valor;
+			contador+=1;
+		}
+		
+		System.out.println("A soma dos números é: "+ soma);
+        
+		System.out.println("Digite a senha");
+		int resposta = scanner.nextInt();
+		
+		while (resposta != 1234) {
+			System.out.println("Senha incorreta");
+			resposta = scanner.nextInt();
+		}
+		
+		System.out.println("Senha correta");
+		
+		/*Peça um valor para o usuário e mostre
+		 * os números do 1 até chegar no número
+		 * que ele digitou
+		 */
+		
+		
+		System.out.println("Informe um valor");
+		int num = scanner.nextInt();
+		
+		while (num >= 1) {
+			System.out.println(num);
+			num-=1;
+		
+        }
+	}
+
+}
