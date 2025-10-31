170f9fd88bcb85c7b1d82d3da8fefdd5fdae59e8
laura flor <lauraflorrodrigues@gmail.com>
2025-10-31 15:51:22 -0300

    Add files via upload

---
+++ encapsulamento/ProdutosP.java
@@ -0,0 +1,10 @@
+package encapsulamento;
+
+public class ProdutosP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ encapsulamento/RetanguloP.java
@@ -0,0 +1,10 @@
+package encapsulamento;
+
+public class RetanguloP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
